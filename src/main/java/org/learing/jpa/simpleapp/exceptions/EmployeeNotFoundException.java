7ed910fa1996ffcff057cf8bedb62d0c985d3f0b
Pavel <gpawel@gmail.com>
2021-05-19 17:25:06 -0400

    Working on Employee controller

--- src/main/java/org/learing/jpa/simpleapp/exceptions/EmployeeNotFoundException.java
+++ src/main/java/org/learing/jpa/simpleapp/exceptions/EmployeeNotFoundException.java
@@ -3,7 +3,7 @@ package org.learing.jpa.simpleapp.exceptions;
 import org.learing.jpa.simpleapp.model.Employee;
 
 public class EmployeeNotFoundException extends RuntimeException{
-    public EmployeeNotFoundException(String s) {
-        super(s);
+    public EmployeeNotFoundException(String message) {
+        super(message);
     }
 }
