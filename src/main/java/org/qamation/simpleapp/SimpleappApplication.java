cc9977ecfc6bb1c96ee0b616b92e34c8c0483ec8
Pavel <gpawel@gmail.com>
2021-08-31 18:29:19 -0400

    Added support for CORS

--- src/main/java/org/qamation/simpleapp/SimpleappApplication.java
+++ src/main/java/org/qamation/simpleapp/SimpleappApplication.java
@@ -20,7 +20,7 @@ public class SimpleappApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","https://pixabay.com"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","https://www.bootdey.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
